e65139933a9f1d7cb5da5bbd2a046fb853c3ea2c
Thạch Nguyễn <thachnh.tesse@gmail.com>
2021-06-03 20:28:49 +0800

    authen history

--- Routes/data.js
+++ Routes/data.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const { getHistory } = require('../controllers/dataController')
+const { authenticateToken } = require('../controllers/tokenController')
+
 
 // Routes
-router.get('/history', getHistory)
+router.get('/history', authenticateToken, getHistory)
 
 
 module.exports = router
